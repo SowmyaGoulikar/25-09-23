56ae29279568f7d301b715f5b550138e13d7ed15
SowmyaGoulikar <146078900+SowmyaGoulikar@users.noreply.github.com>
2023-09-27 18:43:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function Changeimg(){
+    document.getElementById("bangle").src="silverjumkha.jpeg";
+    document.getElementById("bangle").style.padding="15px;"
+    document.getElementById("bangle").style.margin="10px";
+    document.getElementById("bangle").style.border="10px dotted brown";
+}
+function Changeimg1(){
+     document.getElementById("chain").src="silverbangles.jpeg";
+     document.getElementById("chain").style.padding="15px";
+     document.getElementById("chain").style.margin="15px";
+    document.getElementById("chain").style.border="10px double darkmagenta";
+}
+function Changeimg2(){
+     document.getElementById("clrbang").src="multicolorbangles.jpeg";
+     document.getElementById("clrbang").style.padding="15px";
+     document.getElementById("clrbang").style.margin="20px";
+    document.getElementById("clrbang").style.border="10px dashed deeppink";
+}
+function Changeimg3(){
+     document.getElementById("anklet").src="anklets.jpeg";
+     document.getElementById("anklet").style.padding="15px";
+     document.getElementById("anklet").style.margin="20px";
+     document.getElementById("anklet").style.border="10px groove coral";
+}
